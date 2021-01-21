60034c80ac7eb1bd2991bbed7d0a86a10a47df3f
Maksim Rakitin <mrakitin@bnl.gov>
2021-01-21 17:56:49 -0500

    Add a missing file to fix CI

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -3,3 +3,7 @@
 # cp -v <...> ~/.ipython/profile_${TEST_PROFILE}/...
 
 conda install -y -c ${CONDA_CHANNEL_NAME} 03-id-hxn-collection
+
+mkdir -v -p /home/xf03id/
+
+touch /home/xf03id/benchmark.out
