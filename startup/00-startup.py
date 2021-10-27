c9ad5742506a40ee122c9e4f59fd173ccdcf24d3
Maksim Rakitin <mrakitin@bnl.gov>
2021-10-27 14:31:36 -0400

    Use default value for `BLUESKY_KAFKA_BOOTSTRAP_SERVERS`

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -29,7 +29,7 @@ os.environ["PPMAC_HOST"] = "xf03idc-ppmac1"
 
 kafka_publisher = Publisher(
         topic="hxn.bluesky.datum.documents",
-        bootstrap_servers=os.environ['BLUESKY_KAFKA_BOOTSTRAP_SERVERS'],
+        bootstrap_servers=os.getenv("BLUESKY_KAFKA_BOOTSTRAP_SERVERS", "")
         key=str(uuid.uuid4()),
         producer_config={
                 "acks": 1,
