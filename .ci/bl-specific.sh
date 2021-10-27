01e067dba39689014a6a13d913ebe0f980d55f76
Maksim Rakitin <mrakitin@bnl.gov>
2021-10-27 18:34:54 -0400

    CI: install `hxnfly` from the branch with the fixed imports

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# TODO: watch for https://github.com/NSLS-II-HXN/hxnfly/pull/8 to be
+# merged/released, then this install can be removed from here.
+python3 -m pip install --no-deps -vv git+https://github.com/NSLS-II-HXN/hxnfly@change-xspress3-imports
+
 sudo mkdir -v -p /home/xf03id/
 sudo chown -Rv $USER: /home/xf03id/
 touch /home/xf03id/benchmark.out
