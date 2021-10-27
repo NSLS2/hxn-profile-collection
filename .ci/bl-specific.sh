f8753a848e249c46e03a1bcb8d2148c43938d653
Maksim Rakitin <mrakitin@bnl.gov>
2021-10-27 14:37:54 -0400

    CI: do not install bl-specific packages

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# cp -v <...> ~/.ipython/profile_${TEST_PROFILE}/...
-
-conda install -y -c ${CONDA_CHANNEL_NAME} 03-id-hxn-collection
-
 sudo mkdir -v -p /home/xf03id/
 sudo chown -Rv $USER: /home/xf03id/
 touch /home/xf03id/benchmark.out
