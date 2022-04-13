e05a10ab4e98f51ae4c5755392a45c510a1e3dc8
HXN Operator <xf03id@xf03idc-ws1.nsls2.bnl.local>
2022-04-13 13:39:06 -0400

    Docstrings in acceptance tests

--- acceptance_tests/fly1d_zp.py
+++ acceptance_tests/fly1d_zp.py
@@ -1,3 +1,6 @@
 def test_fly1d():
+    """
+    Test ``fly1d`` scan with ZP motor.
+    """
     RE(fly1d([sclr1,zebra,merlin1,xspress3],zpssx,-1,1,10,0.03))
 
--- acceptance_tests/fly2d_zp.py
+++ acceptance_tests/fly2d_zp.py
@@ -1,3 +1,6 @@
 def test_fly2d():
+    """
+    Test ``fly2d`` scan with ZP motors.
+    """
     RE(fly2d([sclr1,zebra,merlin1,xspress3],zpssx,-1,1,10,zpssy,-1,1,10,0.03))
 
