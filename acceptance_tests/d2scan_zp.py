10c33e568830031cc7a3d34034d3490478835d55
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2022-04-14 13:49:28 -0400

    Fixed 'd2scan' acceptance test

--- acceptance_tests/d2scan_zp.py
+++ acceptance_tests/d2scan_zp.py
@@ -2,5 +2,5 @@ def test_d2scan():
     """
     Test ``d2scan`` scan (2D step scan) with ZP motors.
     """
-    RE(d2scan([sclr1,zebra,merlin1,xspress3],zpssx,-1,1,10,zpssy,-1,1,10,0.03))
+    RE(d2scan([sclr1,zebra,merlin1,xspress3],10,zpssx,-1,1,zpssy,-1,1,0.03))
 
