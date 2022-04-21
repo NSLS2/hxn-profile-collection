b637f6667a9e1161e75231d11de76f6651314ae0
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2022-04-21 09:54:12 -0400

    TST: install 'mpl_qtthread'

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -3,6 +3,7 @@
 # TODO: watch for https://github.com/NSLS-II-HXN/hxnfly/pull/8 to be
 # merged/released, then this install can be removed from here.
 python3 -m pip install --no-deps -vv git+https://github.com/NSLS-II-HXN/hxnfly@master
+python3 -m pip install mpl_qtthread
 
 sudo mkdir -v -p /home/xf03id/
 sudo chown -Rv $USER: /home/xf03id/
