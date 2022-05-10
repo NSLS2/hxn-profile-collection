f92f3f0b2d36ec3513bbd95937284e0df899cece
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2022-05-10 09:26:11 -0400

    Use databroker v1 (not v0)

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -43,11 +43,10 @@ EpicsSignalBase.set_defaults(timeout=10, connection_timeout=10)
 # Set up a Broker.
 # TODO clean this up
 from bluesky_kafka import Publisher
-from databroker.v0 import Broker
+from databroker import Broker
 from databroker.assets.mongo import Registry
 from databroker.headersource.core import doc_or_uid_to_uid
 from databroker.headersource.mongo import MDS
-from databroker.v0 import Broker
 from jsonschema import validate as js_validate
 from pymongo import MongoClient
 
